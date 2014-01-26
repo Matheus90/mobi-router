a7c7da029398bc8d9db65659aa980f7dcfb0a0b3
Matheus <matheus@matheus-N56JR.(none)>
2014-01-26 23:57:37 +0100

    Mobi-Router first push

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+    summary: "Simple router for mobile displaying with sidebar"
+});
+
+Package.on_use(function (api, where) {
+    api.use(['templating', 'handlebars'], 'client');
+
+    api.add_files([
+        'lib/js/_touch.js',
+        'lib/js/animation.js',
+        'lib/js/easing.js',
+        'lib/js/inheritance.js',
+        'lib/js/iscroll.js',
+        'lib/js/resizeable.js',
+        'lib/helpers.js',
+        'lib/mobi_route.js',
+        'lib/mobi_router.css',
+        'lib/mobi_router.js',
+        'lib/mobi_sequence.js',
+        'lib/templates.html',
+        'lib/templates.js'
+    ], 'client');
+
+    if (api.export)
+        api.export('MobiRouter');
+});
