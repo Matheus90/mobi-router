cbc60fc040f0dcbbe42a0238ac806cee538b6df3
Charanjit Singh <ckhabra007@gmail.com>
2014-09-05 20:27:52 +0530

    Ugraded to meteor 0.9.1. Please publish with 'meteor publish' as I am no longer authorised to do so

--- package.js
+++ package.js
@@ -1,39 +1,44 @@
 Package.describe({
-    summary: "Simple router made for meteor apps designed specifically for mobile devices"
+  summary: "Simple router made for meteor apps designed specifically for mobile devices",
+  "version": "0.6.57",
+  "git": "https://github.com/Matheus90/mobi-router.git",
+  "name": "matheus90:mobi-router"
 });
 
 Package.on_use(function (api, where) {
-    api.use(['templating', 'handlebars', 'deps', 'session'], ['client', 'server']);
+  api.versionsFrom && api.versionsFrom("METEOR@0.9.1");
+
+  api.use(['templating', 'handlebars', 'deps', 'session'], ['client', 'server']);
 
 
-    api.add_files([
-        'lib/js/slog.js'
-    ], ['client', 'server']);
+  api.add_files([
+    'lib/js/slog.js'
+  ], ['client', 'server']);
 
-    api.add_files([
-        'lib/js/touch.js',
-        //'lib/js/animation.js',
-        'lib/js/animation2.js',
-        'lib/js/easing.js',
-        'lib/js/clickbuster.js',
-        'lib/js/inheritance.js',
-        'lib/js/iscroll.js',
-        //'lib/js/iscroll-5-probe.js',
-        'lib/js/resizeable.js',
-        'lib/viewtypes/detailview.html',
-        'lib/viewtypes/detailview.js',
-        'lib/viewtypes/tableview.html',
-        'lib/viewtypes/tableview.js',
-        'lib/viewtypes/tableview.css',
-        'lib/helpers.js',
-        'lib/mobi_route.js',
-        'lib/mobi_router.css',
-        'lib/mobi_router.js',
-        'lib/templates.html',
-        'lib/templates.js'
-    ], 'client');
+  api.add_files([
+    'lib/js/touch.js',
+    //'lib/js/animation.js',
+    'lib/js/animation2.js',
+    'lib/js/easing.js',
+    'lib/js/clickbuster.js',
+    'lib/js/inheritance.js',
+    'lib/js/iscroll.js',
+    //'lib/js/iscroll-5-probe.js',
+    'lib/js/resizeable.js',
+    'lib/viewtypes/detailview.html',
+    'lib/viewtypes/detailview.js',
+    'lib/viewtypes/tableview.html',
+    'lib/viewtypes/tableview.js',
+    'lib/viewtypes/tableview.css',
+    'lib/helpers.js',
+    'lib/mobi_route.js',
+    'lib/mobi_router.css',
+    'lib/mobi_router.js',
+    'lib/templates.html',
+    'lib/templates.js'
+  ], 'client');
 
 
-    if (api.export)
-        api.export(['MobiRouter','MobiResizeable','ClickBuster','slog','EV']);
+  if (api.export)
+    api.export(['MobiRouter','MobiResizeable','ClickBuster','slog','EV']);
 });
